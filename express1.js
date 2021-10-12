dc1e01e00773eeb89d41131920935a82d44c07a9
sumitthukral <91280421+sumitthukral@users.noreply.github.com>
2021-10-12 01:35:18 -0700

    Delete express1.js

--- express1.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const port = 3000;
-const app = express()
-app.get('/',(req,res)=>{
-    res.send('hello world is working')
-})
-app.listen(port,()=>{
-    console.log('port is listening now')
-})
