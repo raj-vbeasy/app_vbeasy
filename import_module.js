ba77212b3cced4057515994b4a30abff2be0ab69
sumitthukral <91280421+sumitthukral@users.noreply.github.com>
2021-10-12 01:36:57 -0700

    Delete import_module.js

--- import_module.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const file = require('./import/file.js');
-console.log(sumit);
-//writting file into json file 
-fs.writeFileSync('exported.json',JSON.stringify(sumit),()=>{
-    if(data){
-        console.log('data write successfully');
-    }
-});
