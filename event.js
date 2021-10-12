5bcfd1280265e0b356ccfa350fe58832678e5592
sumitthukral <91280421+sumitthukral@users.noreply.github.com>
2021-10-12 01:34:09 -0700

    Delete event.js

--- event.js
+++
@@ -1,9 +0,0 @@
-const myEmitter = require('events');
-class MyEmit extends myEmitter{}
-const myemit = new MyEmit();
-myemit.on('waterfull',()=>{
-    setTimeout(()=>{
-        console.log('please turn off the motor water tank is full');
-    },2000);
-});
-myemit.emit('waterfull');
