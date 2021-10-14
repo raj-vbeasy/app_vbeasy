899e77bc68d9115c8091dd023121291d609a1ed0
sumitthukral <91280421+sumitthukral@users.noreply.github.com>
2021-10-14 02:47:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-const http = require('http');
-const fs = require('fs');
-const  port = process.env.port || 8080;
-const server = http.createServer((req,res)=>{
-  const home = fs.readFileSync('/public/index.html');
-  if(req.url == '/home')
-  {
-    res.statusCode = 200;
-    res.setHeader('content-type','text/html');
-    res.end(home);
-  }
-  else
-  {
-    res.statusCode = 404;
-    res.end('404  error data not found');
-  }
-});
+console.log('hello world');
