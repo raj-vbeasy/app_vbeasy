9135c554561c6c56277b55246add58f0fad1675c
sumitthukral <91280421+sumitthukral@users.noreply.github.com>
2021-10-14 00:47:24 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+const  port = process.env.port;
+const server = http.createServer((req,res)=>{
+  const home = fs.readFileSync('/public/index.html');
+  if(req.url == '/home')
+  {
+    res.statusCode = 200;
+    res.setHeader('content-type','text/html');
+    res.end(home);
+  }
+  else
+  {
+    res.statusCode = 404;
+    res.end('404  error data not found');
+  }
+});
